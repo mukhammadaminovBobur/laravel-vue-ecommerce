fb0025402dec0b2fe97c29f084cee221f92a6f56
Muhammadbobur Muhammaddaminov <virtualcoder20@gmail.com>
2022-11-29 18:09:55 +0100

    Password reset form and layout for unauthorized users

--- backend/src/views/RequestPassword.vue
+++ backend/src/views/RequestPassword.vue
@@ -1,5 +1,5 @@
 <template>
-  <GuestLayout title="Sign in to your account">
+  <GuestLayout title="Request new password">
     <form class="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" value="true" />
       <div class="-space-y-px rounded-md shadow-sm">
@@ -7,21 +7,12 @@
           <label for="email-address" class="sr-only">Email address</label>
           <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
         </div>
-        <div>
-          <label for="password" class="sr-only">Password</label>
-          <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
-        </div>
       </div>
 
       <div class="flex items-center justify-between">
-        <div class="flex items-center">
-          <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-          <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-        </div>
-
         <div class="text-sm">
-          <router-link to="/request-password" class="font-medium text-indigo-600 hover:text-indigo-500">
-            Forgot your password?
+          <router-link to="/login" class="font-medium text-indigo-600 hover:text-indigo-500">
+            Remember your password?
           </router-link>
         </div>
       </div>
@@ -31,7 +22,7 @@
             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
               <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
             </span>
-          Sign in
+          Submit
         </button>
       </div>
     </form>
