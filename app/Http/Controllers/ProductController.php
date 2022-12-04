ac556f703de10ae4b606dd4e1b0ec54f8b940bcf
Muhammadbobur Muhammaddaminov <virtualcoder20@gmail.com>
2022-12-04 14:05:32 +0100

    Product crud part 1

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductListResource;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of resource
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index()
+    {
+        $perPage = request('per_page', 10);
+        $search = \request('search', '');
+        $sortField = \request('sort_field', 'updated_at');
+        $sortDirection = \request('sort_direction', 'desc');
+
+        $query = Product::query()
+            ->where('title', 'like', '%{$search}%')
+            ->orderBy($sortField, $sortDirection)
+            ->paginate($perPage);
+
+        return ProductListResource::collection($query);
+    }
+
+    /**
+     * Store a newly created resource in storage
+     * @param Request
+     * $return Response
+     */
+    public function store(ProductRequest $request)
+    {
+        $data = $request->validated();
+        $data['created_by'] = $request->user()->id;
+        $data['updated_by'] = $request->user()->id;
+
+        /** @var UploadedFile $image */
+        $image = $data['image'] ?? null;
+        //check if image was given and saved on local file system
+        if ($image){
+            $relativePath = $this->saveImage($image);
+            $data['image'] = URL::to(Storage::url($relativePath));
+            $data['image_mime'] = $image->getClientMimeType();
+            $data['image_size'] = $image->getSize();
+        }
+
+        $product = Product::create($data);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display specified resource
+     *
+     * @param Product $product
+     * @return ProductResource
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    /**
+     * Update specified resource in storage
+     *
+     * @param Request $request
+     * @param Product $product
+     * @return ProductResource
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        $data = $request->validated();
+        $data['updated_by'] = $request->user()->id;
+
+        /** @var UploadedFile $image */
+        $image = $data['image'] ?? null;
+        //check if image was given and saved on local file system
+        if ($image){
+            $relativePath = $this->saveImage($image);
+            $data['image'] = URL::to(Storage::url($relativePath));
+            $data['image_mime'] = $image->getClientMimeType();
+            $data['image_size'] = $image->getSize();
+
+            //If there is an old image, delete it
+            if ($product->image){
+                Storage::deleteDirectory('/public' . dirname($product->image));
+            }
+        }
+
+        $product->update($data);
+
+        return new ProductResource($product);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return \response()->noContent();
+    }
+
+
+    private function saveImage(UploadedFile $image)
+    {
+        $path = 'images/' . Str::random();
+        if (!Storage::exists($path)) {
+            Storage::makeDirectory($path, 0755, true);
+        }
+        if (!Storage::putFileAS('public/' . $path, $image, $image->getClientOriginalName())) {
+            throw new \Exception("Unable to save file \"{$image->getClientOriginalName()}\"");
+        }
+
+        return $path . '/' . $image->getClientOriginalName();
+    }
+}
