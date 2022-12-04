777c55895874c714a6e8abf4e347af007347d7a3
Muhammadbobur Muhammaddaminov <virtualcoder20@gmail.com>
2022-12-04 12:14:55 +0100

    App layout changed

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -1,65 +1,80 @@
 <template>
-  <div class="flex">
+  <div class="min-h-full">
+    <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="flex h-16 items-center justify-between">
+          <div class="flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+            </div>
+            <div class="hidden md:block">
+              <div class="ml-10 flex items-baseline space-x-4">
+                <router-link v-for="item in navigations" :key="item.name" :to="item.to" :class="[this.$route.name === item.to.name  ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" >{{ item.name }}</router-link>
+              </div>
+            </div>
+          </div>
+          <div class="hidden md:block">
+            <div class="ml-4 flex items-center md:ml-6">
+              <button type="button" class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span class="sr-only">View notifications</span>
+                <BellIcon class="h-6 w-6" aria-hidden="true" />
+              </button>
 
-    <nav class="main-menu text-blue-100 bg-blue-800 h-screen flex flex-col justify-between w-3/12">
-
-      <div class="nav-top p-3">
-        <ul class="flex flex-col space-y-2 p-4">
-          <li v-for="navigation in navigations" :class="[this.$route.name === navigation.to.name ? 'bg-blue-900' : 'hover:bg-blue-900', 'cursor-pointer p-2 rounded-lg transition-bg duration-500 ease-out']">
-            <router-link :to="navigation.to" class="flex">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-              </svg>
-              <span class="nav-text">
-                {{ navigation.name }}
-              </span>
-            </router-link>
-          </li>
-
-        </ul>
-
-      </div>
-
-      <ul class="logout p-4">
-        <li class="cursor-pointer hover:bg-blue-900 p-2 rounded-lg transition-bg duration-500 ease-out" @click="logout">
-          <a class="flex">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 pr-1">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M5.636 5.636a9 9 0 1012.728 0M12 3v9" />
-            </svg>
-
-            <span class="nav-text">
-              Logout
-            </span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-
-    <div id="main-content" class="bg-gray-100 w-full flex flex-col">
-
-      <div id="search-header" class="shadow h-16 px-4 flex items-center justify-between text-gray-600">
-
-        <div class="capitalize">
-          <i class="fa fa-search mr-4"></i>
-          search
-        </div>
-
-        <div class="flex items-center">
-          <i class="fa fa-bell mr-4"></i>
-          <img src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="user avatar" class="rounded-full h-6" />
-
+              <!-- Profile dropdown -->
+              <Menu as="div" class="relative ml-3">
+                <div>
+                  <MenuButton class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span class="sr-only">Open user menu</span>
+                    <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                  </MenuButton>
+                </div>
+                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                  <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                      <a @click="logout(item.name)" :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                    </MenuItem>
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </div>
+          </div>
+          <div class="-mr-2 flex md:hidden">
+            <!-- Mobile menu button -->
+            <DisclosureButton class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
         </div>
       </div>
 
+      <DisclosurePanel class="md:hidden">
+        <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+          <router-link v-for="item in navigations" :key="item.name" as="a" :to="item.to" :class="[this.$route.name === item.to.name ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']">{{ item.name }}</router-link>
+        </div>
+        <div class="border-t border-gray-700 pt-4 pb-3">
+          <div class="flex items-center px-5">
+            <div class="flex-shrink-0">
+              <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+            </div>
+            <div class="ml-3">
+              <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
+              <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
+            </div>
+            <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+              <span class="sr-only">View notifications</span>
+              <BellIcon class="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div class="mt-3 space-y-1 px-2">
+            <DisclosureButton v-for="item in userNavigation" :key="item.name" @click="logout(item.name)" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">{{ item.name }}</DisclosureButton>
+          </div>
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
 
-
-
-      <div id="content-area" class=" bg-gray-300 text-gray-800 p-8 h-full">
-        <router-view></router-view>
-      </div>
-
-    </div>
-
+    <router-view></router-view>
   </div>
 </template>
 
@@ -67,12 +82,28 @@
 
   import router from "../router/index.js";
   import store from "../store/index.js";
+  import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+  import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 
-  function logout(){
-    store.dispatch('logout')
-      .then(() =>{
-        router.push({name:'login'})
-      })
+  const user = {
+    name: 'Tom Cook',
+    email: 'tom@example.com',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  }
+  const userNavigation = [
+    { name: 'Your Profile', href: '#' },
+    { name: 'Settings', href: '#' },
+    { name: 'Sign out', href: '#' },
+  ]
+
+  function logout(name){
+    if (typeof(name) == 'string' && name == "Sign out"){
+      store.dispatch('logout')
+        .then(() =>{
+          router.push({name:'login'})
+        })
+    }
   }
 
   const navigations = [
--- backend/src/views/Products.vue
+++ backend/src/views/Products.vue
@@ -1,10 +1,9 @@
 <template>
-  <h2 class="capitalize text-2xl font-semibold text-blue-800"> Products </h2>
-  <div class="h-64 border-2 border-blue-800 rounded border-dashed p-4 my-4">
-    {{this.$route.name}}
-  </div>
+  <PageComponent title="Products">
+
+  </PageComponent>
 </template>
 
 <script setup>
-
+import PageComponent from "../components/PageComponent.vue";
 </script>
